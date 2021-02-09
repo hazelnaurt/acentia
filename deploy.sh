70f7b26c310979fbf2956c7b8cb6f715aed1e7f3
Hazelnaurt <hazelnaurt@gmail.com>
2021-02-09 10:53:19 +0000

    Staged old changes

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #acentia.tgz
-tar -czf acentia_u1.tgz public/* AccentiaEnergy-app-config.json package.json package-lock.json acentiaenergy.com.conf mail.acentiaenergy.com.conf server.js app.js
+tar -czf update2a.tgz public/about.html public/contact.html public/index.html public/service.html
+#tar -czf acentia_u2.tgz public/about.html public/index.html public/service.html public/contact.html 
 
-scp -P 2642 -i ~/.ssh/id_rsa_acentia acentia_u1.tgz webaccentia@213.219.37.251:~
+scp -i ~/.ssh/id_rsa_acentiae update2a.tgz daniel@213.219.37.251:~
 
-rm -rf acentia_u1.tgz;
+#rm -rf acentia_u2.tgz;
 
 # ssh -i ~/.ssh/id_rsa_acentia webaccentia@213.219.37.251 << 'ENDSSH'
 
